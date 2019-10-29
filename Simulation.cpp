ec037817d1b763a2c7decc3ca3d0858fbe47d0ba
Dawid Sopata <dsopata@saba.com>
2019-10-29 13:10:12 +0100

    rollback to init version

--- Simulation.cpp
+++ Simulation.cpp
@@ -6,79 +6,71 @@
 
 using namespace std;
 
-int comSize;
-
-Simulation::Simulation(long _size, MyMPI *_mmpi) {
+Simulation::Simulation( long _size, MyMPI *_mmpi ) {
     size = _size;
     initPositions();
     mmpi = _mmpi;
-    mmpi->MPI_Comm_size( MPI_COMM_WORLD, &comSize );
 }
 
-void Simulation::setCumulativeProbability(double **cp) {
+void Simulation::setCumulativeProbability( double **cp ) {
     cumulativeProbability = cp;
 }
 
 // polozenie poczatkowe to (0,0)
 void Simulation::initPositions() {
-    x = new int[size];
-    y = new int[size];
+    x = new int[ size ];
+    y = new int[ size ];
 
-    for (long i = 0; i < size; i++)
-        x[i] = y[i] = 0;
+    for ( long i = 0; i < size; i++ )
+        x[ i ] = y[ i ] = 0;
 }
 
 void Simulation::init() {
     // w wersji sekwencyjnej nie ma tu nic ciekawego
-//    MPI_SCA
 }
 
-long Simulation::distanceSQ(long i, long j) {
-    long dx = x[i] - x[j];
-    long dy = y[i] - y[j];
-    return dx * dx + dy * dy;
+long Simulation::distanceSQ( long i, long j ) {
+    long dx = x[ i ] - x[ j ];
+    long dy = y[ i ] - y[ j ];
+    return dx * dx  + dy * dy;
 }
 
 // co sie dzieje z i-ta czastka
-int Simulation::whichOne(long i) {
+int Simulation::whichOne( long i ) {
     double r = drand48();
 
 //   cout << "i " << i << endl;
-    for (int j = 0; j < 5; j++) {// stop + 4 kierunki
+    for ( int j = 0; j < 5; j++ ) {// stop + 4 kierunki
 //      cout << " j " << j << " " << cumulativeProbability[ i ][ j ] << endl;
-        if (r < cumulativeProbability[i][j]) return j;
+        if ( r < cumulativeProbability[ i ][ j ] ) return j;
     }
     return 4;
 }
 
-void Simulation::move(long i, int option) {
-    if (option == 0) return; // stoimy w miejscu
+void Simulation::move( long i, int option ) {
+    if ( option == 0 ) return; // stoimy w miejscu
 
-    switch (option) {
-        case 1:
-            y[i]++; // w gore
+    switch ( option ) {
+        case 1: y[i]++; // w gore
             break;
-        case 2:
-            x[i]++; // w prawo
+        case 2: x[i]++; // w prawo
             break;
-        case 3:
-            y[i]--; // w dol
+        case 3: y[i]--; // w dol
             break;
-        case 4:
-            x[i]--; // w lewo
+        case 4: x[i]--; // w lewo
             break;
     }
 }
 
-void Simulation::doIt(int steps) {
-    for (int step = 0; step < steps; step++)
-        for (long i = 0; i < size; i++)
-            move(i, whichOne(i));
+void Simulation::doIt( int steps  ) {
+    for ( int step = 0; step < steps; step++ )
+        for ( long i = 0; i < size; i++ )
+            move( i, whichOne( i ) );
 }
 
 double Simulation::avgX() {
     double avg = 0;
-    for (long i = 0; i < size; i++)
+    for ( long i = 0; i < size; i++ )
         avg += x[i];
 
     return avg / size;
@@ -86,7 +78,7 @@ double Simulation::avgX() {
 
 double Simulation::avgY() {
     double avg = 0;
-    for (long i = 0; i < size; i++)
+    for ( long i = 0; i < size; i++ )
         avg += y[i];
 
     return avg / size;
@@ -96,43 +88,39 @@ double Simulation::avgY() {
 // odległość od min do max włącznie.
 // tą część kodu można napisać lepiej, ale
 // tak będzie łatwiej uzyskać wyższą efektywność.
-void Simulation::calcAvgNumberOfPointsBetween(int min, int max) {
+void Simulation::calcAvgNumberOfPointsBetween( int min, int max ) {
     long minSQ = min * min;
     long maxSQ = max * max;
 
     long numberOfPointsBetween = 0;
     long dSQ;
 
-
-    cout << comSize << endl;
-
     // dla każdej cząstki
-    for (int i = 0; i < size; i++) {
+    for ( int i = 0; i < size; i++ ) {
         // przeglądamy jej sąsiadów
-        for (int j = 0; j < size; j++) {
+        for ( int j = 0; j < size; j++ ) {
             // omijając tu spotkanie cząstki z samą sobą
-            if (i != j) {
+            if ( i != j ) {
                 dSQ = distanceSQ(i, j);
                 // zliczamy tych, których odległość zgadza się
                 // z zapytaniem
-                if ((dSQ >= minSQ) && (dSQ <= maxSQ))
+                if ( ( dSQ >= minSQ ) && ( dSQ <= maxSQ ) )
                     numberOfPointsBetween++;
             }
         }
     }
 
-    avgNumberOfPointsBetween = (double) numberOfPointsBetween / (double) size;
+    avgNumberOfPointsBetween = (double)numberOfPointsBetween / (double)size;
 }
 
 double Simulation::getAvgNumberOfPointsBetween() {
     return avgNumberOfPointsBetween;
 }
 
-ostream &operator<<(ostream &o, const Simulation &s) {
+ostream & operator<< ( ostream &o, const Simulation &s ) {
     o << "Points # " << s.size << endl;
-    for (int i = 0; i < s.size; i++) {
-        o << setw(5) << i << " " << setprecision(4) << fixed << setw(7) << s.x[i] << " " << right << setw(7) << s.y[i]
-          << endl;
+    for ( int i = 0; i < s.size; i++ ) {
+        o << setw( 5 ) << i << " " << setprecision(4) << fixed << setw( 7 ) << s.x[i] << " " << right << setw( 7 ) << s.y[ i ] << endl;
     }
     return o;
 }
--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include<iostream>
 #include<iomanip>
 #include"Simulation.h"
-#include <mpi.h>
 
 using namespace std;
 
@@ -9,7 +8,7 @@ void calcCummulative( double *probability ) {
     double sum = 0.0;
     for ( int i = 0; i < 5; i++ )
         sum += probability[ i ];
-    
+
     for ( int i = 0; i < 5; i++ )
         probability[ i ] /= sum;
 
@@ -35,24 +34,23 @@ int main( int ac, char **av ) {
     long particles = 50000;
     int repetitions = 10;
 
-	MyMPI *mmpi = new MyMPI();
-	mmpi->MPI_Init( &ac, &av );
+    MyMPI *mmpi = new MyMPI();
+    mmpi->MPI_Init( &ac, &av );
 
     Simulation *s = new Simulation( particles, mmpi );
 
-	int myRank, mySize;
-	mmpi->MPI_Comm_rank( MPI_COMM_WORLD, &myRank );
-
+    int myRank;
+    mmpi->MPI_Comm_rank( MPI_COMM_WORLD, &myRank );
 
     if ( myRank == 0 ) {
-        double pattern1[ 5 ] = { 2, 1.005, 1, 1, 1 }; 
-        double pattern2[ 5 ] = { 2, 0.995, 1, 1, 1 }; 
+        double pattern1[ 5 ] = { 2, 1.005, 1, 1, 1 };
+        double pattern2[ 5 ] = { 2, 0.995, 1, 1, 1 };
 
         calcCummulative( pattern1 );
         calcCummulative( pattern2 );
 
 // alokacja pamięci
-        double ** probability4particles = new double * [ particles ]; 
+        double ** probability4particles = new double * [ particles ];
         for ( long i = 0; i < particles; i++ ) {
             probability4particles[ i ] = new double [5];
         }
@@ -93,11 +91,11 @@ int main( int ac, char **av ) {
         }
 
         s->calcAvgNumberOfPointsBetween(0,500);
-        
+
         if ( myRank == 0 ) {
             v1 = s->getAvgNumberOfPointsBetween();
         }
-        
+
         s->calcAvgNumberOfPointsBetween(501,20000);
 
         if ( myRank == 0 ) {
@@ -105,8 +103,8 @@ int main( int ac, char **av ) {
             total_sum += mmpi->MPI_Wtime() - dT_start;
 
             v2 = s->getAvgNumberOfPointsBetween();
-            cout << " <x> = " << setiosflags (ios::showbase) << setw(7) << setprecision(3) << s->avgX() 
-                 << " <y> = " << setiosflags (ios::showbase) << setw(7) << setprecision(3) << s->avgY() 
+            cout << " <x> = " << setiosflags (ios::showbase) << setw(7) << setprecision(3) << s->avgX()
+                 << " <y> = " << setiosflags (ios::showbase) << setw(7) << setprecision(3) << s->avgY()
                  << " v1 = "  << setiosflags (ios::showbase | ios::showpoint | ios::fixed) << setw(8) << setprecision(1) << v1
                  << " v2 = "  << setiosflags (ios::showbase | ios::showpoint | ios::fixed) << setw(8) << setprecision(1) << v2 << endl;
         }
@@ -119,6 +117,5 @@ int main( int ac, char **av ) {
         cout << "Total avg time ........................  " << total_sum / repetitions << endl;
     }
 
-	mmpi->MPI_Finalize();
+    mmpi->MPI_Finalize();
 }
-
